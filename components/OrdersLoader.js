5a92f71a128ab35b954146611809ec46f9a251fa
John Elmo Johnson <elmoj6@gmail.com>
2022-02-11 10:35:01 +1100

    Added key to prevent deployment issues

--- components/OrdersLoader.js
+++ components/OrdersLoader.js
@@ -5,7 +5,7 @@ const OrdersLoader = () => {
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
       {[...Array(3)].map((_, index) => {
         return (
-          <div className="p-4 bg-slate-200 h-28 rounded-lg animate-pulse" />
+          <div key={index} className="p-4 bg-slate-200 h-28 rounded-lg animate-pulse" />
         );
       })}
     </div>
